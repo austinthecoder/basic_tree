605eb2b47e30fc4805dead4ac15777f5992afdb4
Austin Schneider <soccer022483@gmail.com>
2010-12-21 12:50:15 -0500

    remove jeweler code

--- lib/basic_tree.rb
+++ lib/basic_tree.rb
@@ -1,14 +1,16 @@
 class BasicTree
-  
+
   include Enumerable
-  
+
+  VERSION = "0.1.1"
+
   def initialize(object, &block)
     self.object = object
     yield self if block_given?
   end
-  
+
   attr_accessor :object, :parent
-  
+
   def add(object, &block)
     self.class.new(object) do |child|
       children << child
@@ -16,45 +18,45 @@ class BasicTree
       yield child if block_given?
     end
   end
-  
+
   def path
     ancestors << self
   end
-  
+
   def ancestors
     root? ? [] : (parent.ancestors << parent)
   end
-  
+
   def descendants
     children.map { |c| [c] + c.descendants }.flatten
   end
-  
+
   def subtree
     [self] + descendants
   end
-  
+
   def siblings
     root? ? [] : parent.children.select { |child| child != self }
   end
-  
+
   def root
     path.first
   end
-  
+
   def level
     path.size
   end
-  
+
   def root?
     !parent
   end
-  
+
   def leaf?
     children.empty?
   end
-  
+
   def children
     @children ||= []
   end
-  
+
 end
